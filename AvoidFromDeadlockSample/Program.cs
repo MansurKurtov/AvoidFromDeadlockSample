f2f42e360ea887759e19d2f2d9fadbba2cbc4eed
Mansur <kurtov.mansur@gmail.com>
2021-09-26 17:59:14 +0500

    Some changes

--- AvoidFromDeadlockSample/Program.cs
+++ AvoidFromDeadlockSample/Program.cs
@@ -9,37 +9,51 @@ namespace AvoidFromDeadlockSample
 {
     class Program
     {
-        static object obj1 = new object();
-        static object obj2 = new object();
+
 
         static void Main(string[] args)
         {
+            object obj1 = new object();
             var task1 = new Task(() =>
             {
                 Console.WriteLine("Task1 started");
 
-                lock (obj1)
-                {
-                    Thread.Sleep(100);
-                    lock (obj2)
+                //lock (obj1)
+                //{
+                //    Thread.Sleep(100);
+                //    lock (obj2)
+                //    {
+                //        Console.WriteLine("Task1 finished");
+                //    }
+                //}
+                if (Monitor.TryEnter(obj1, 10))
+                    try
                     {
+                        Thread.Sleep(100);
                         Console.WriteLine("Task1 finished");
                     }
-                }
+                    finally
+                    {
+                        Monitor.Exit(obj1);
+                    }
             });
             var task2 = new Task(() =>
             {
                 Console.WriteLine("Task2 started");
 
-                lock (obj2)
-                {
-                    Thread.Sleep(100);
-                    lock (obj1)
+                if (Monitor.TryEnter(obj1, 100))
+                    try
                     {
+                        //Thread.Sleep(100);
+
                         Console.WriteLine("Task2 finished");
                     }
-                }
+                    finally
+                    {
+                        Monitor.Exit(obj1);
+                    }
             });
+
             Task.WaitAll(task1, task2);
             Console.WriteLine("All tasks finished");
         }
